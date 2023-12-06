72944f36a61e8749bc41f3556b7fc7c0de7bb1a9
Byekibe <kibetpeter95@gmail.com>
2023-12-06 04:31:17 +0300

    Flask app

---
+++ backend/server.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+port = os.getenv("PORT")
+
+
+@app.route("/api")
+def home():
+    names = ["Kia", "Kai", "Koa", "Kei", "Kui"]
+    ages = [12, 10, 15, 9, 5]
+    heights = (1.5, 1.2, 1.7, 1, 0.75)
+    return {
+        "names": names,
+        "ages": ages,
+        "heights": heights
+    }, 200
+
+@app.route("/api/about")
+def about():
+    str = "A simple Flask App"
+    return str, 200
+
+
+if __name__=="__main__":
+    app.run(port=port, debug=True)
